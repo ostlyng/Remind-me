20a729fbaf459dba6f15dc852fd211601b8b7093
Thomas Østlyng <t.ostlyng@icloud.com>
2018-10-07 11:42:19 +0200

    Initial Commit

---
+++ Remind me/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remind me
+//
+//  Created by Anne Kristine on 07/10/2018.
+//  Copyright © 2018 Thomas Østlyng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
